38e9a4adc6b8d294dc5b2def67c3e6df0f4c3443
Mark Schill <Mark.Schill@cmschill.net>
2016-06-26 19:34:57 -0400

    Updated version number.

--- plugin/spigot/src/main/java/com/splunk/spigot/LogToSplunkPlugin.java
+++ plugin/spigot/src/main/java/com/splunk/spigot/LogToSplunkPlugin.java
@@ -18,7 +18,7 @@ import com.splunk.spigot.eventloggers.PlayerEventLogger;
 
 public class LogToSplunkPlugin extends JavaPlugin implements Listener {
     public static final String MODID = "logtosplunk";
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.1-SNAPSHOT";
     public static final String NAME = "Splunk for Minecraft";
     public static final String SPLUNK_PROPERTIES = "/config/splunk.properties";
 
