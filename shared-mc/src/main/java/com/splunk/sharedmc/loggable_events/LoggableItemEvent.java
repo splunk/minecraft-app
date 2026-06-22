93ca7863457d6235ca9a0bb9d5170d6b8170d44b
splunk <masao.jeffery@gmail.com>
2026-06-22 14:53:20 -0400

    feat(shared-mc): add LoggableItemEvent and LoggableProgressionEvent

---
+++ shared-mc/src/main/java/com/splunk/sharedmc/loggable_events/LoggableItemEvent.java
@@ -0,0 +1,38 @@
+package com.splunk.sharedmc.loggable_events;
+
+import com.splunk.sharedmc.Point3dLong;
+
+/**
+ * Item economy events: pickup and drop.
+ */
+public class LoggableItemEvent extends AbstractLoggableEvent {
+
+    public LoggableItemEvent(ItemAction action, long gameTime, String worldName, Point3dLong location) {
+        super(LoggableEventType.ITEM, gameTime, worldName, location);
+        this.addField(ACTION, action.asString());
+    }
+
+    public LoggableItemEvent setPlayerName(String playerName) {
+        this.addField(PLAYER_NAME, playerName);
+        return this;
+    }
+
+    public LoggableItemEvent setItem(String item) {
+        this.addField("item", item);
+        return this;
+    }
+
+    public LoggableItemEvent setQuantity(int quantity) {
+        this.addField("quantity", quantity);
+        return this;
+    }
+
+    public enum ItemAction {
+        PICKUP("pickup"),
+        DROP("drop");
+
+        private final String action;
+        ItemAction(String action) { this.action = action; }
+        public String asString() { return action; }
+    }
+}
