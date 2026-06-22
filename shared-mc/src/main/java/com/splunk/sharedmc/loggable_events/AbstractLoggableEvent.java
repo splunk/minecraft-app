5cc182f2f7262747e9db2d701869e6fc44c86936
splunk <masao.jeffery@gmail.com>
2026-06-22 14:45:12 -0400

    feat(shared-mc): add SERVER/PERFORMANCE/COMBAT/ITEM/PROGRESSION types + location-less event ctor

--- shared-mc/src/main/java/com/splunk/sharedmc/loggable_events/AbstractLoggableEvent.java
+++ shared-mc/src/main/java/com/splunk/sharedmc/loggable_events/AbstractLoggableEvent.java
@@ -21,18 +21,24 @@ public class AbstractLoggableEvent extends SplunkCimLogEvent implements Loggable
      * @param type The type of event that this is.
      */
     public AbstractLoggableEvent(LoggableEventType type, long worldTime, String worldName, Point3dLong coordinates) {
+        this(type, worldTime, worldName);
+        if (coordinates != null) {
+            this.addField("xCoord", coordinates.xCoord);
+            this.addField("yCoord", coordinates.yCoord);
+            this.addField("zCoord", coordinates.zCoord);
+        }
+    }
+
+    /**
+     * Constructor for events with no world location (e.g. server lifecycle, performance).
+     */
+    public AbstractLoggableEvent(LoggableEventType type, long worldTime, String worldName) {
         super(type.getEventName(), "");
- 
         this.addField("time", System.currentTimeMillis());
-
-
         this.addField("game_time", worldTime);
-        if(worldName != null) {
+        if (worldName != null) {
             this.addField("world", worldName);
         }
-        this.addField("xCoord", coordinates.xCoord);
-        this.addField("yCoord", coordinates.yCoord);
-        this.addField("zCoord", coordinates.zCoord);
     }
 
     @Override
